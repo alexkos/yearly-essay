3e3f1315497d7784b99d39b2dc25c687dde684a4
Alexandr Kosarevsky <alexkos37@gmail.com>
2012-05-03 17:53:37 +0400

    change body

--- proxy_client.py
+++ proxy_client.py
@@ -4,10 +4,11 @@ from tornado import httpclient
 from tornado import ioloop 
 
 def handle_request(request):
-    request_client = httpclient.HTTPRequest(url=request.uri, method=request.method, body=request.body, headers=request.headers)
+    request_client = httpclient.HTTPRequest(url=request.uri, method=request.method, body=request.body or None, headers=request.headers)
     http_client = httpclient.HTTPClient()
     response = http_client.fetch(request_client)
-    print(request_client)
+    print(response)
+    print(response.body)
 
 http_server = httpserver.HTTPServer(handle_request)
 http_server.listen(8080)
