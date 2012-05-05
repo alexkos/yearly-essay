fb58618b841fd0d0ecf783e35fa5c15dfa6cc59d
Alexandr Kosarevsky <alexkos37@gmail.com>
2012-05-05 18:10:13 +0700

    curl_httpclient

--- proxy_client.py
+++ proxy_client.py
@@ -1,14 +1,16 @@
 from tornado import web
 from tornado import httpserver
-from tornado import httpclient
+from tornado.httpclient import HTTPRequest
 from tornado import ioloop 
+from tornado.curl_httpclient import CurlAsyncHTTPClient
 
 def handle_request(request):
-    request_client = httpclient.HTTPRequest(url=request.uri, method=request.method, body=request.body or None, \
-        headers=request.headers, proxy_host = '127.0.0.1', proxy_port = '8888')
-    http_client = httpclient.AsyncHTTPClient()
-    http_client.configure("tornado.curl_httpclient.CurlAsyncHTTPClient")
-    response = http_client.fetch(request_client)
+    def response_client():
+        print response
+    request_client = HTTPRequest(url=request.uri, method=request.method, body=request.body or None, \
+        headers=request.headers, proxy_host = '127.0.0.1', proxy_port = 8888)
+    http_client = CurlAsyncHTTPClient()
+    response = http_client.fetch(request_client,response_client)
 
 http_server = httpserver.HTTPServer(handle_request)
 http_server.listen(8080)
