b294c21650d6cf7a5bbcf1fa91c543bc5be45038
Alexandr Kosarevsky <alexkos37@gmail.com>
2012-05-05 12:40:26 +0700

    asynchttp

--- proxy_client.py
+++ proxy_client.py
@@ -6,9 +6,11 @@ from tornado import ioloop
 def handle_request(request):
     request_client = httpclient.HTTPRequest(url=request.uri, method=request.method, body=request.body or None, \
         headers=request.headers, proxy_host = '127.0.0.1', proxy_port = '8888')
-    http_client = httpclient.HTTPClient()
+    http_client = httpclient.AsyncHTTPClient()
+    http_client.configure("tornado.curl_httpclient.CurlAsyncHTTPClient")
     response = http_client.fetch(request_client)
 
 http_server = httpserver.HTTPServer(handle_request)
 http_server.listen(8080)
 ioloop.IOLoop.instance().start()
+
