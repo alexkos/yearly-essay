5adf48b73b7e5a88718a582b02c44df0463d9fc6
Alexandr Kosarevsky <alexkos37@gmail.com>
2012-05-08 00:15:41 +0400

    edit method

--- proxy_client.py
+++ proxy_client.py
@@ -5,16 +5,16 @@ from tornado import ioloop
 from tornado.curl_httpclient import CurlAsyncHTTPClient
 
 class ProxyClient(web.RequestHandler):
+    def response_client(self, response):
+        self.write(response.body)
+        self.finish()
 
     @web.asynchronous
     def get(self):
-        def response_client(response):
-            self.write(response.body)
-            self.finish()
         request_client = HTTPRequest(url = self.request.uri, method = self.request.method, body = self.request.body or None, \
             headers = self.request.headers, proxy_host = '127.0.0.1', proxy_port = 8888)
         http_client = CurlAsyncHTTPClient()
-        response = http_client.fetch(request_client,response_client)
+        response = http_client.fetch(request_client, self.response_client)
 
 app = web.Application([(r'.*', ProxyClient),])
 http_server = httpserver.HTTPServer(app)
--- proxy_server.py
+++ proxy_server.py
@@ -5,15 +5,15 @@ from tornado import ioloop
 from tornado.curl_httpclient import CurlAsyncHTTPClient
 
 class ProxyServer(web.RequestHandler):
+    def response_client(self, response):
+        self.write(response.body)
+        self.finish()
 
     @web.asynchronous
     def get(self):
-        def response_client(response):
-            self.write(response.body)
-            self.finish()
         request_client = HTTPRequest(url = self.request.uri, method = self.request.method, body = self.request.body or None, headers = self.request.headers)
         http_client = CurlAsyncHTTPClient()
-        response = http_client.fetch(request_client,response_client)
+        response = http_client.fetch(request_client, self.response_client)
 
 app = web.Application([(r'.*', ProxyServer),])
 http_server = httpserver.HTTPServer(app)
