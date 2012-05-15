440763c0d2ebac54a8292e3efc8ad42f892da899
Alexandr Kosarevsky <alexkos37@gmail.com>
2012-05-16 01:16:16 +0700

    encrypt no ended

--- proxy_client.py
+++ proxy_client.py
@@ -1,4 +1,8 @@
+import os
+import base64
 import logging
+import M2Crypto
+from Crypto.Cipher import AES
 from tornado import options
 from tornado import web
 from tornado import httpserver
@@ -7,19 +11,32 @@ from tornado import ioloop
 from tornado.curl_httpclient import CurlAsyncHTTPClient
 
 class ProxyClient(web.RequestHandler):
+
+    def gen_key_aes(self, enc_body):
+        blocksize = 32
+        secret_key = os.urandom(blocksize)
+        cipher = AES.new(secret)
+        return cipher
+
     def response_client(self, response):
-        self.write(response.body)
+        #decoded_body = DecodeAES(cipher, response.body)
+        self.write(decoded_body)
         self.finish()
 
     @web.asynchronous
     def get(self):
+        #WriteProxy_client = M2Crypto.RSA.load_pub_key('proxy_server-public.key')
+        #EncryptKey = WriteProxy_server.public_encrypt(self.gen_key_aes(), M2Crypto.RSA.pkcs1_oaep_padding)
+        self.add_header('x-Encrypt', 'EncryptKey')
         request_client = HTTPRequest(url = self.request.uri, method = self.request.method, body = self.request.body or None, \
             headers = self.request.headers, proxy_host = '127.0.0.1', proxy_port = 8888)
+        print(request_client.headers)
         http_client = CurlAsyncHTTPClient()
         response = http_client.fetch(request_client, self.response_client)
 
 options.parse_command_line()
 logging.info('Client server started @ 127.0.0.1:8080 -> proxy server: 127.0.0.0:8888')
+
 app = web.Application([(r'.*', ProxyClient),])
 http_server = httpserver.HTTPServer(app)
 http_server.listen(8080)
--- proxy_server.py
+++ proxy_server.py
@@ -1,4 +1,8 @@
+import os
+import base64
 import logging
+import M2Crypto
+from Crypto.Cipher import AES
 from tornado import options
 from tornado import web
 from tornado import httpserver
@@ -7,18 +11,34 @@ from tornado import ioloop
 from tornado.curl_httpclient import CurlAsyncHTTPClient
 
 class ProxyServer(web.RequestHandler):
+    def encrypt(self, cipher, enc_body):
+        padding = '{'
+        pad = lambda s: s + (blocksize - len(s) % blocksize) * padding
+        EncodeAES = lambda c, s: base64.b64encode(c.encrypt(pad(s)))
+        encoded = EncodeAES(cipher, enc_body)
+        return encoded
+
     def response_client(self, response):
-        self.write(response.body)
+        #ReadProxy_server = M2Crypto.RSA.load_key ('proxy_server-private.key')
+        #key_aes = ReadProxy_server.private_decrypt ('getting encrypt key', M2Crypto.RSA.pkcs1_oaep_padding)
+        self.write(self.encrypt(key_aes, response.body))
         self.finish()
 
     @web.asynchronous
     def get(self):
-        request_client = HTTPRequest(url = self.request.uri, method = self.request.method, body = self.request.body or None, headers = self.request.headers)
+        request_client = HTTPRequest(url = self.request.uri, method = self.request.method, body = self.request.body or None, \
+            headers = self.request.headers)
         http_client = CurlAsyncHTTPClient()
         response = http_client.fetch(request_client, self.response_client)
 
+#M2Crypto.Rand.rand_seed(os.urandom(1024))
+#proxy_server = M2Crypto.RSA.gen_key(1024, 65537)
+#proxy_server.save_key ('proxy_server-private.key', None)
+#proxy_server.save_pub_key ('proxy_server-public.key')
+
 options.parse_command_line()
 logging.info('Proxy server started @ 127.0.0.0:8888')
+
 app = web.Application([(r'.*', ProxyServer),])
 http_server = httpserver.HTTPServer(app)
 http_server.listen(8888)
