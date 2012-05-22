6ead133ee40a468dc23c4743f2528cc1dd5091a8
Alexandr Kosarevsky <alexkos37@gmail.com>
2012-05-22 20:07:31 +0700

    add check digital signature

--- proxy_client.py
+++ proxy_client.py
@@ -15,7 +15,8 @@ define("host", default= 'localhost', help="run on the given port", type=str)
 define("port", default=8080, help="run on the given port", type=int)
 define("host_proxy", default= 'localhost', help="run on the given port", type=str)
 define("port_proxy", default=8888, help="run on the given port", type=int)
-define("proxy_client-private_key", default='proxy_client-private.key', help="run on the encrypt body", type=str)
+define("client_private_key", default='proxy_client-private.key', help="run on the encrypt body", type=str)
+define("server_public_key", default='proxy_server-private.key', help="run on the encrypt body", type=str)
 
 class ProxyClient(web.RequestHandler):
 
@@ -29,14 +30,33 @@ class ProxyClient(web.RequestHandler):
 
     def decrypt_key_aes(self, encrypt_key):
         decode_key = base64.b64decode(encrypt_key)
-        secret_key = M2Crypto.RSA.load_key (options.key)
+        secret_key = M2Crypto.RSA.load_key (options.client_private_key)
         key_aes = secret_key.private_decrypt (decode_key, M2Crypto.RSA.pkcs1_oaep_padding)
 
         return key_aes
 
-    def response_client(self, response):
+    def check_signature(self, decsign, checksign):
+        check_sign = False
+        sign = base64.b64decode(decsign)
+        key = base64.b64decode(checksign)
+        secretkey = M2Crypto.RSA.load_key (options.server_public_key)
+        chkey = secretkey.private_decrypt(sign, M2Crypto.RSA.pkcs1_oaep_padding)
+
+        if chkey == key:
+            check_sign = True
+
+        return check_sign
+
+    def response_client(self, response):        
         if 'x-Encrypt' in response.headers:
-            self.write(self.decrypt_body(self.decrypt_key_aes(response.headers['x-Encrypt']), response))
+            Encrypt = response.headers['x-Encrypt']
+            Signature = response.headers['x-Signature']
+            
+            check = self.check_signature(Signature,Encrypt)
+            if check:
+                self.write(self.decrypt_body(self.decrypt_key_aes(Encrypt),response))
+            else:
+                self.write('THIS IS NOT CORRECTLY CONTENT')
         else:
             self.write(response.body)
         if self._headers.has_key('Content-Type'):
